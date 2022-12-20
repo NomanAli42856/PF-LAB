9f6780d18298966342715eb9ffc73f69a5654c24
NomanAli42856 <121042827+NomanAli42856@users.noreply.github.com>
2022-12-20 18:34:58 +0500

    Add files via upload

---
+++ creativityC.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+main()
+{
+system("Color 9F");
+system("cls");
+cout << "              .::---::.  "<< endl; 
+cout << "           .-------------.  "<< endl;
+cout << "         .-----------------.  "<< endl;
+cout << "        .---------------.  "<< endl;
+cout << "        .-------------.  "<< endl;
+cout << "       .------------.  "<< endl;
+cout << "        .-------------.  "<< endl;
+cout << "         .----------------.  "<< endl;
+cout << "         .------------------.  "<< endl;
+cout << "           .--------------.  "<< endl;
+cout << "              .::---::.  "<< endl; 
+
+}
